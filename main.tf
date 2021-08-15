161b9e94ee299c8361fc2d929511a2fdc5cbec3b
jyablonski <jyablonski9@gmail.com>
2021-08-15 13:08:40 -0500

    tf updates

--- main.tf
+++ main.tf
@@ -13,6 +13,11 @@ variable "secret_key"{
     sensitive = true
 }
 
+variable "jacobs_cidr_block"{
+    type = list(string)
+    sensitive = true
+}
+
 provider "aws" {
     region = var.region
     access_key = var.access_key
@@ -43,38 +48,47 @@ resource "aws_s3_bucket" "jacobs_bucket_tf" {
   }
 }
 
-# resource "aws_security_group" "jacobs_rds_security_group_tf" {
-#   name        = "jacobs_security_group for rds"
-#   description = "Allow Jacob's Traffic to RDS"
-#   vpc_id      = aws_vpc.jacobs_vpc_tf.id # needs to get changed
+resource "aws_security_group" "jacobs_rds_security_group_tf" {
+  name        = "jacobs_security_group for rds"
+  description = "Allow Jacobs Traffic to RDS"
+  vpc_id      = aws_default_vpc.jacobs_vpc_tf.id # needs to get changed
 
-#   # inbound rules to let these resources ACCESS / WRITE TO the database.
-#   # need to put in like 7 different ip addresses
-#   # TO DO - Complete inbound access rules.
+  # inbound rules to let these resources ACCESS / WRITE TO the database.
+  # need to put in like 7 different ip addresses
+  # TO DO - Complete inbound access rules.
   
-#   ingress {
-#     description      = "Inbound rules for RDS DB"
-#     from_port        = 3306
-#     to_port          = 3306
-#     protocol         = "tcp"
-#     cidr_blocks      = [aws_vpc.jacobs_vpc_tf.cidr_block]
-#     ipv6_cidr_blocks = [aws_vpc.jacobs_vpc_tf.ipv6_cidr_block]
-#   }
+  ingress {
+    description      = "Custom IP Addresses"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = var.jacobs_cidr_block
 
-#   # outbound
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+  }
 
-#   tags = {
-#     Name        = local.env_name
-#     Environment = local.env_type
-#   }
-# }
+  ingress {
+    description      = "Other Security Groups"
+    from_port        = -1
+    to_port          = -1
+    protocol         = "all"
+    security_groups  = ["sg-edbb7ff3"] # create a separate SG here for only aws lambda/ecs objects to access so they can connect to RDS
+  }
+
+
+  # outbound
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name        = local.env_name
+    Environment = local.env_type
+  }
+}
 
 # resource "aws_db_instance" "jacobs_rds_tf" {
 #   allocated_storage    = 20
