fb9ed944bce735e38c0a7a2706c0536029cfd7bf
jyablonski <jyablonski9@gmail.com>
2021-08-15 15:14:23 -0500

    rds complete

--- main.tf
+++ main.tf
@@ -18,6 +18,16 @@ variable "jacobs_cidr_block"{
     sensitive = true
 }
 
+variable "jacobs_rds_user" {
+    type = string
+    sensitive = true
+}
+
+variable "jacobs_rds_pw" {
+    type = string
+    sensitive = true
+}
+
 provider "aws" {
     region = var.region
     access_key = var.access_key
@@ -48,15 +58,39 @@ resource "aws_s3_bucket" "jacobs_bucket_tf" {
   }
 }
 
+# attach this to things like aws lambda or ecs tasks so they can connect to the rds database
+resource "aws_security_group" "jacobs_task_security_group_tf"{
+    name = "jacobs_security_group for tasks"
+    description = "Connect Tasks to RDS"
+    vpc_id = aws_default_vpc.jacobs_vpc_tf.id
+
+    ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name        = local.env_name
+    Environment = local.env_type
+  }
+}
+
 resource "aws_security_group" "jacobs_rds_security_group_tf" {
   name        = "jacobs_security_group for rds"
   description = "Allow Jacobs Traffic to RDS"
-  vpc_id      = aws_default_vpc.jacobs_vpc_tf.id # needs to get changed
+  vpc_id      = aws_default_vpc.jacobs_vpc_tf.id
 
-  # inbound rules to let these resources ACCESS / WRITE TO the database.
-  # need to put in like 7 different ip addresses
-  # TO DO - Complete inbound access rules.
-  
   ingress {
     description      = "Custom IP Addresses"
     from_port        = 3306
@@ -71,7 +105,7 @@ resource "aws_security_group" "jacobs_rds_security_group_tf" {
     from_port        = -1
     to_port          = -1
     protocol         = "all"
-    security_groups  = ["sg-edbb7ff3"] # create a separate SG here for only aws lambda/ecs objects to access so they can connect to RDS
+    security_groups  = [aws_security_group.jacobs_task_security_group_tf.id]
   }
 
 
@@ -90,24 +124,26 @@ resource "aws_security_group" "jacobs_rds_security_group_tf" {
   }
 }
 
-# resource "aws_db_instance" "jacobs_rds_tf" {
-#   allocated_storage    = 20
-#   max_allocated_storage = 21
-#   engine               = "mysql"
-#   engine_version       = "8.0" # try this or 8.0.23
-#   instance_class       = "db.t2.micro"
-#   port                 = 3306
-#   name                 = "jacob_db"
-#   username             = "user"
-#   password             = "jacobpass123"
-#   parameter_group_name = "default.mysql8.0.25" # try this
-#   skip_final_snapshot  = true
-#   publicly_accessible  = false
-#   storage_type         = "gp2" # general purpose ssd
+resource "aws_db_instance" "jacobs_rds_tf" {
+  allocated_storage    = 20
+  max_allocated_storage = 21
+  engine               = "mysql"
+  engine_version       = "8.0" # try this or 8.0.23
+  instance_class       = "db.t2.micro"
+  identifier = "jacobs-rds-server"
+  port                 = 3306
+  name                 = "jacob_db"
+  username             = var.jacobs_rds_user
+  password             = var.jacobs_rds_pw
+  # parameter_group_name = "default.mysql8.0.25" # try this
+  skip_final_snapshot  = true
+  publicly_accessible  = true
+  storage_type         = "gp2" # general purpose ssd
+  vpc_security_group_ids = [aws_security_group.jacobs_rds_security_group_tf.id]
 
-#   tags = {
-#     Name        = local.env_name
-#     Environment = local.env_type
-#   }
+  tags = {
+    Name        = local.env_name
+    Environment = local.env_type
+  }
 
-# }
+}
