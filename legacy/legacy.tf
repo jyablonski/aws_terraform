96b8b3a6e2c3b06d587ee1411944d4d582ac856e
jyablonski <jyablonski9@gmail.com>
2021-08-21 14:49:22 -0500

    completely functional automated python scraper

---
+++ legacy/legacy.tf
@@ -0,0 +1,228 @@
+#### gateways, route tables, subnets for lambda functionality
+
+# resource "aws_subnet" "jacobs_public_subnet" {
+#   vpc_id     = aws_vpc.jacobs_vpc_tf.id
+#   cidr_block = cidrsubnet(aws_vpc.jacobs_vpc_tf.cidr_block, 8, 1)
+#   map_public_ip_on_launch = true
+
+#   tags = {
+#     Name        = "Jacobs Public Subnet"
+#     Environment = local.env_type
+#   }
+# }
+
+# resource "aws_subnet" "jacobs_private_subnet" {
+#   vpc_id     = aws_vpc.jacobs_vpc_tf.id
+#   cidr_block = cidrsubnet(aws_vpc.jacobs_vpc_tf.cidr_block, 8, 2)
+
+#   tags = {
+#     Name        = "Jacobs Private Subnet"
+#     Environment = local.env_type
+#   }
+# }
+
+# resource "aws_subnet" "jacobs_public_subnet_2" {
+#   vpc_id     = aws_vpc.jacobs_vpc_tf.id
+#   cidr_block = cidrsubnet(aws_vpc.jacobs_vpc_tf.cidr_block, 8, 3)
+#   map_public_ip_on_launch = true
+
+#   tags = {
+#     Name        = "Jacobs Public Subnet 2"
+#     Environment = local.env_type
+#   }
+# }
+
+# resource "aws_db_subnet_group" "jacobs_subnet_group" {
+#   name = "jacobs-subnet-group"
+#   subnet_ids = [aws_subnet.jacobs_public_subnet.id, aws_subnet.jacobs_public_subnet_2.id]
+
+#   tags = {
+#     Name        = local.env_name
+#     Environment = local.env_type
+#   }
+# }
+
+# resource "aws_internet_gateway" "jacobs_gw" {
+#   vpc_id = aws_vpc.jacobs_vpc_tf.id
+
+#   tags = {
+#     Name        = "Jacobs Gateway"
+#     Environment = local.env_type
+#   }
+# }
+
+# resource "aws_eip" "jacobs_eip" {
+#   vpc = true
+#   public_ipv4_pool = "amazon"
+#   depends_on                = [aws_internet_gateway.jacobs_gw]
+# }
+
+# resource "aws_network_interface" "jacobs_network_interface" {
+#   subnet_id       = aws_subnet.jacobs_public_subnet.id
+#   # private_ips     = ["10.0.0.50"] # idk what to put here or how to make it automatic 
+
+#   tags = {
+#     Name        = "Jacobs Network Interface"
+#     Environment = local.env_type
+#   }
+# }
+
+# resource "aws_nat_gateway" "jacobs_nat_gw" {
+#   allocation_id = aws_eip.jacobs_eip.id
+#   subnet_id     = aws_subnet.jacobs_public_subnet.id
+#   # network_interface_id = aws_network_interface.jacobs_network_interface.id
+
+#   tags = {
+#     Name        = "Jacobs NAT Gateway"
+#     Environment = local.env_type
+#   }
+
+#   depends_on = [aws_internet_gateway.jacobs_gw]
+# }
+# # Elastic IP address [eipalloc-063efa619a6e80514] is already associated
+
+
+
+
+
+# resource "aws_route_table" "jacobs_private_route_table" {
+#   vpc_id = aws_vpc.jacobs_vpc_tf.id
+
+#   route = [
+#     {
+#       cidr_block = "0.0.0.0/0"
+#       nat_gateway_id = aws_nat_gateway.jacobs_nat_gw.id
+#       carrier_gateway_id = ""
+#       destination_prefix_list_id = ""
+#       egress_only_gateway_id = ""
+#       instance_id = ""
+#       ipv6_cidr_block = ""
+#       local_gateway_id = ""
+#       gateway_id = ""
+#       network_interface_id = ""
+#       transit_gateway_id = ""
+#       vpc_endpoint_id = ""
+#       vpc_peering_connection_id = ""
+#     }
+#   ]
+
+#   tags = {
+#     Name        = "Jacobs Private Route Table"
+#     Environment = local.env_type
+#   }
+# }
+
+# resource "aws_route_table" "jacobs_public_route_table" {
+#   vpc_id = aws_vpc.jacobs_vpc_tf.id
+
+#   route = [
+#     {
+#       cidr_block = "0.0.0.0/0"
+#       gateway_id = aws_internet_gateway.jacobs_gw.id
+#       carrier_gateway_id = ""
+#       destination_prefix_list_id = ""
+#       egress_only_gateway_id = ""
+#       instance_id = ""
+#       ipv6_cidr_block = ""
+#       local_gateway_id = ""
+#       nat_gateway_id = ""
+#       network_interface_id = ""
+#       transit_gateway_id = ""
+#       vpc_endpoint_id = ""
+#       vpc_peering_connection_id = ""
+#     }
+#   ]
+#   tags = {
+#     Name        = "Jacobs Public Route Table"
+#     Environment = local.env_type
+#   }
+# }
+
+# resource "aws_route_table_association" "jacobs_private_route" {
+#   subnet_id      = aws_subnet.jacobs_private_subnet.id
+#   route_table_id = aws_route_table.jacobs_private_route_table.id
+
+
+# }
+
+# resource "aws_route_table_association" "jacobs_public_route" {
+#   subnet_id      = aws_subnet.jacobs_public_subnet.id
+#   route_table_id = aws_route_table.jacobs_public_route_table.id
+
+# }
+
+#### i had to manually verify my email in SES, dont think there's a programmatic way to validate yourself ???
+resource "aws_ses_email_identity" "jacobs_email" {
+  email = var.jacobs_email_address
+}
+
+# ARNS for lambda role
+# arn:aws:iam::aws:policy/AmazonSESFullAccess
+# arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole
+
+# i'm still figuring out assume_role_policy, has to do with the `TRUSTED_ENTITIES` part in IAM.  it's not some default policy, it points to what general area the iam role is.
+# only 1 assume role per role
+# 
+resource "aws_iam_role" "jacobs_lambda_role" {
+  name = "jacobs_lambda_role"
+  description = "Role created for AWS ECS"
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [{
+        "Effect": "Allow",
+        "Principal": {
+            "Service": "lambda.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+    }]
+}
+EOF
+}
+
+# hardcoding aws managed policies seems to be fine / ok practice.
+resource "aws_iam_role_policy_attachment" "jacobs_iam_role_update_lambda" {
+  role       = aws_iam_role.jacobs_lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "jacobs_iam_role_update_ses" {
+  role       = aws_iam_role.jacobs_lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSESFullAccess"
+}
+
+# resource "aws_lambda_function" "jacobs_lambda" {
+#   function_name = "Python Scraper"
+#   runtime = "python3.8"
+#   memory_size = 256
+#   timeout = 20
+#   role = aws_iam_role.jacobs_lambda_role.arn
+#   handler = "lambda_function2.lambda_handler"
+#   filename = "C:/Users/Jacob/Documents/python_aws/lambda_function2.py"
+#   s3_bucket = "https://jacobsbucket97.s3.amazonaws.com/"
+#   s3_key = "lambda_function2.py"
+#   layers = ["arn:aws:lambda:us-east-1:770693421928:layer:Klayers-python38-pandas:38",
+#             "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-python38-lxml:6",
+#             "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-python38-PyMySQL:4",
+#             "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-python38-SQLAlchemy:27",
+#             "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-python38-beautifulsoup4:10"]
+#    environment {
+#     variables = {
+#       IP = aws_db_instance.jacobs_rds_tf.address
+#       PORT = aws_db_instance.jacobs_rds_tf.port
+#       RDS_DB = "jacob_db"
+#       RDS_PW = var.jacobs_rds_pw
+#       RDS_USER = var.jacobs_rds_user
+#     }
+#   }
+
+#     vpc_config {
+#       security_group_ids = [aws_security_group.jacobs_task_security_group_tf.id]
+#       subnet_ids = [aws_subnet.jacobs_private_subnet.id]
+#     }
+
+# tags = {
+#     Name        = local.env_name
+#     Environment = local.env_type
+#   }
+# }
--- prod/main.tf
+++ prod/main.tf
@@ -12,6 +12,7 @@ locals {
 
 resource "aws_vpc" "jacobs_vpc_tf" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
 
   tags = {
     Name        = "Jacobs VPC"
@@ -95,34 +96,6 @@ resource "aws_security_group" "jacobs_rds_security_group_tf" {
   }
 }
 
-resource "aws_db_instance" "jacobs_rds_tf" {
-  allocated_storage    = 20
-  max_allocated_storage = 21
-  engine               = "mysql"
-  engine_version       = "8.0" # try this or 8.0.23
-  instance_class       = "db.t2.micro"
-  identifier = "jacobs-rds-server"
-  port                 = 3306
-  name                 = "jacob_db"   # this is the name of the default database that will be created.
-  username             = var.jacobs_rds_user
-  password             = var.jacobs_rds_pw
-  # parameter_group_name = "default.mysql8.0.25" # try this
-  skip_final_snapshot  = true
-  publicly_accessible  = true
-  storage_type         = "gp2" # general purpose ssd
-  vpc_security_group_ids = [aws_security_group.jacobs_rds_security_group_tf.id]
-  # db_subnet_group_name = aws_db_subnet_group.jacobs_subnet_group.name
-
-  tags = {
-    Name        = local.env_name
-    Environment = local.env_type
-  }
-
-}
-
-# comment lines 121 - 238
-#### gateways, route tables, subnets for lambda functionality
-
 resource "aws_subnet" "jacobs_public_subnet" {
   vpc_id     = aws_vpc.jacobs_vpc_tf.id
   cidr_block = cidrsubnet(aws_vpc.jacobs_vpc_tf.cidr_block, 8, 1)
@@ -130,16 +103,6 @@ resource "aws_subnet" "jacobs_public_subnet" {
 
   tags = {
     Name        = "Jacobs Public Subnet"
-    Environment = local.env_type
-  }
-}
-
-resource "aws_subnet" "jacobs_private_subnet" {
-  vpc_id     = aws_vpc.jacobs_vpc_tf.id
-  cidr_block = cidrsubnet(aws_vpc.jacobs_vpc_tf.cidr_block, 8, 2)
-
-  tags = {
-    Name        = "Jacobs Private Subnet"
     Environment = local.env_type
   }
 }
@@ -174,67 +137,6 @@ resource "aws_internet_gateway" "jacobs_gw" {
   }
 }
 
-resource "aws_eip" "jacobs_eip" {
-  vpc = true
-  public_ipv4_pool = "amazon"
-  depends_on                = [aws_internet_gateway.jacobs_gw]
-}
-
-resource "aws_network_interface" "jacobs_network_interface" {
-  subnet_id       = aws_subnet.jacobs_public_subnet.id
-  # private_ips     = ["10.0.0.50"] # idk what to put here or how to make it automatic 
-
-  tags = {
-    Name        = "Jacobs Network Interface"
-    Environment = local.env_type
-  }
-}
-
-resource "aws_nat_gateway" "jacobs_nat_gw" {
-  allocation_id = aws_eip.jacobs_eip.id
-  subnet_id     = aws_subnet.jacobs_public_subnet.id
-  # network_interface_id = aws_network_interface.jacobs_network_interface.id
-
-  tags = {
-    Name        = "Jacobs NAT Gateway"
-    Environment = local.env_type
-  }
-
-  depends_on = [aws_internet_gateway.jacobs_gw]
-}
-# Elastic IP address [eipalloc-063efa619a6e80514] is already associated
-
-
-
-
-
-resource "aws_route_table" "jacobs_private_route_table" {
-  vpc_id = aws_vpc.jacobs_vpc_tf.id
-
-  route = [
-    {
-      cidr_block = "0.0.0.0/0"
-      nat_gateway_id = aws_nat_gateway.jacobs_nat_gw.id
-      carrier_gateway_id = ""
-      destination_prefix_list_id = ""
-      egress_only_gateway_id = ""
-      instance_id = ""
-      ipv6_cidr_block = ""
-      local_gateway_id = ""
-      gateway_id = ""
-      network_interface_id = ""
-      transit_gateway_id = ""
-      vpc_endpoint_id = ""
-      vpc_peering_connection_id = ""
-    }
-  ]
-
-  tags = {
-    Name        = "Jacobs Private Route Table"
-    Environment = local.env_type
-  }
-}
-
 resource "aws_route_table" "jacobs_public_route_table" {
   vpc_id = aws_vpc.jacobs_vpc_tf.id
 
@@ -261,91 +163,179 @@ resource "aws_route_table" "jacobs_public_route_table" {
   }
 }
 
-resource "aws_route_table_association" "jacobs_private_route" {
-  subnet_id      = aws_subnet.jacobs_private_subnet.id
-  route_table_id = aws_route_table.jacobs_private_route_table.id
-
-
-}
-
 resource "aws_route_table_association" "jacobs_public_route" {
   subnet_id      = aws_subnet.jacobs_public_subnet.id
   route_table_id = aws_route_table.jacobs_public_route_table.id
 
 }
 
-#### i had to manually verify my email in SES, dont think there's a programmatic way to validate yourself ???
-resource "aws_ses_email_identity" "jacobs_email" {
-  email = var.jacobs_email_address
+resource "aws_route_table_association" "jacobs_public_route_2" {
+  subnet_id      = aws_subnet.jacobs_public_subnet_2.id
+  route_table_id = aws_route_table.jacobs_public_route_table.id
+
 }
 
-# ARNS for lambda role
-# arn:aws:iam::aws:policy/AmazonSESFullAccess
-# arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole
+resource "aws_db_instance" "jacobs_rds_tf" {
+  allocated_storage    = 20
+  max_allocated_storage = 21
+  engine               = "mysql"
+  engine_version       = "8.0" # try this or 8.0.23
+  instance_class       = "db.t2.micro"
+  identifier = "jacobs-rds-server"
+  port                 = 3306
+  name                 = "jacob_db"   # this is the name of the default database that will be created.
+  username             = var.jacobs_rds_user
+  password             = var.jacobs_rds_pw
+  # parameter_group_name = "default.mysql8.0.25" # try this
+  skip_final_snapshot  = true
+  publicly_accessible  = true
+  storage_type         = "gp2" # general purpose ssd
+  vpc_security_group_ids = [aws_security_group.jacobs_rds_security_group_tf.id]
+  db_subnet_group_name = aws_db_subnet_group.jacobs_subnet_group.id
 
-# i'm still figuring out assume_role_policy, has to do with the `TRUSTED_ENTITIES` part in IAM.  it's not some default policy, it points to what general area the iam role is.
-# only 1 assume role per role
-# 
-resource "aws_iam_role" "jacobs_lambda_role" {
-  name = "jacobs_lambda_role"
+  tags = {
+    Name        = local.env_name
+    Environment = local.env_type
+  }
+
+}
+
+resource "aws_iam_role" "jacobs_ecs_role" {
+  name = "jacobs_ecs_role"
   description = "Role created for AWS ECS"
   assume_role_policy = <<EOF
 {
-    "Version": "2012-10-17",
-    "Statement": [{
-        "Effect": "Allow",
-        "Principal": {
-            "Service": "lambda.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-    }]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
 }
 EOF
+  
 }
 
-# hardcoding aws managed policies seems to be fine / ok practice.
-resource "aws_iam_role_policy_attachment" "jacobs_iam_role_update_lambda" {
-  role       = aws_iam_role.jacobs_lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+resource "aws_iam_role_policy_attachment" "jacobs_ecs_role_attachment" {
+  role       = aws_iam_role.jacobs_ecs_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "jacobs_iam_role_update_ses" {
-  role       = aws_iam_role.jacobs_lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSESFullAccess"
+resource "aws_iam_role" "jacobs_ecs_ecr_role" {
+  name = "jacobs_ecs_ecr_role"
+  description = "Role created for AWS ECS ECR Access"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "events.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+  
 }
 
-# resource "aws_lambda_function" "jacobs_lambda" {
-#   function_name = "Python Scraper"
-#   runtime = "python3.8"
-#   memory_size = 256
-#   timeout = 20
-#   role = aws_iam_role.jacobs_lambda_role.arn
-#   handler = "lambda_function2.lambda_handler"
-#   filename = "C:/Users/Jacob/Documents/python_aws/lambda_function2.py"
-#   s3_bucket = "https://jacobsbucket97.s3.amazonaws.com/"
-#   s3_key = "lambda_function2.py"
-#   layers = ["arn:aws:lambda:us-east-1:770693421928:layer:Klayers-python38-pandas:38",
-#             "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-python38-lxml:6",
-#             "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-python38-PyMySQL:4",
-#             "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-python38-SQLAlchemy:27",
-#             "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-python38-beautifulsoup4:10"]
-#    environment {
-#     variables = {
-#       IP = aws_db_instance.jacobs_rds_tf.address
-#       PORT = aws_db_instance.jacobs_rds_tf.port
-#       RDS_DB = "jacob_db"
-#       RDS_PW = var.jacobs_rds_pw
-#       RDS_USER = var.jacobs_rds_user
-#     }
-#   }
+resource "aws_iam_role_policy_attachment" "jacobs_ecs_ecr_role_attachment" {
+  role       = aws_iam_role.jacobs_ecs_ecr_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceEventsRole"
+}
 
-#     vpc_config {
-#       security_group_ids = [aws_security_group.jacobs_task_security_group_tf.id]
-#       subnet_ids = [aws_subnet.jacobs_private_subnet.id]
-#     }
+# this is a private repo by default.  use aws_ecrpublic_repository for a public repo.
+resource "aws_ecr_repository" "jacobs_repo" {
+  name                 = "jacobs_repo"
+  image_tag_mutability = "MUTABLE"
 
-# tags = {
-#     Name        = local.env_name
-#     Environment = local.env_type
-#   }
-# }
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+## STOP at this step and upload docker image to this repo.  format below
+# docker tag jacobs-python-docker-2:latest 324816727452.dkr.ecr.us-east-1.amazonaws.com/jacobs_repo:latest
+# docker push 324816727452.dkr.ecr.us-east-1.amazonaws.com/jacobs_repo:latest
+
+resource "aws_cloudwatch_log_group" "aws_ecs_logs" {
+  name = "jacobs_ecs_logs"
+
+}
+
+resource "aws_ecs_cluster" "jacobs_ecs_cluster" {
+  name = "jacobs_fargate_cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+# cloudwatch log stuff would go in the container defintion part with logConfiguration
+resource "aws_ecs_task_definition" "jacobs_ecs_task" {
+  family                = "jacobs_task"
+  container_definitions = <<TASK_DEFINITION
+[
+    {
+        "image": "324816727452.dkr.ecr.us-east-1.amazonaws.com/jacobs_repo:latest",
+        "name": "jacobs_container",
+        "environment": [
+          {"name": "IP", "value": "${aws_db_instance.jacobs_rds_tf.address}"},
+          {"name": "PORT", "value": "3306"},
+          {"name": "RDS_USER", "value": "${var.jacobs_rds_user}"},
+          {"name": "RDS_PW", "value": "${var.jacobs_rds_pw}"},
+          {"name": "RDS_DB", "value": "jacob_db"},
+          {"name": "testing_1", "value": "test1"}
+        ],
+        "logConfiguration": {
+          "logDriver": "awslogs",
+          "options": {
+            "awslogs-group": "jacobs_ecs_logs",
+            "awslogs-region": "us-east-1",
+            "awslogs-stream-prefix": "ecs"
+          }
+        }
+    } 
+]
+TASK_DEFINITION
+  execution_role_arn = aws_iam_role.jacobs_ecs_role.arn # aws managed role to give permission to private ecr repo i just made.
+  task_role_arn = aws_iam_role.jacobs_ecs_role.arn
+  cpu = 256
+  memory = 512
+  network_mode = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+}
+
+resource "aws_cloudwatch_event_rule" "every_15_mins" {
+  name = "python_scheduled_task" # change this name
+  description = "Run every 15 minutes"
+  schedule_expression = "cron(0/15 * * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "ecs_scheduled_task" {
+  target_id = "jacobs_target_id"
+  arn = aws_ecs_cluster.jacobs_ecs_cluster.arn
+  rule = aws_cloudwatch_event_rule.every_15_mins.name
+  role_arn  = aws_iam_role.jacobs_ecs_ecr_role.arn
+
+  ecs_target {
+    launch_type = "FARGATE"
+    network_configuration {
+      subnets = [aws_subnet.jacobs_public_subnet.id, aws_subnet.jacobs_public_subnet_2.id] # do not use subnet group here - wont work.  need list of the individual subnet ids.
+      security_groups = [aws_security_group.jacobs_task_security_group_tf.id]
+      assign_public_ip = true
+    }
+    platform_version = "LATEST"
+    task_count = 1
+    task_definition_arn = aws_ecs_task_definition.jacobs_ecs_task.arn
+  }
+}
