e792915e9e678453fc5af36b14b2e10625295e87
jyablonski9 <jyablonski9@gmail.com>
2021-12-03 20:23:55 -0600

    adding airflow dag trigger skele

--- prod/files/main.py
+++ prod/files/main.py
@@ -1,6 +1,7 @@
 import json
 import urllib.parse
 import boto3
+# from botocore.vendored import requests
 
 print('Loading function')
 
@@ -24,3 +25,27 @@ def lambda_handler(event, context):
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
         raise e
+
+
+###################
+#                 #
+#  AIRFLOW LAMBDA #
+#                 #
+###################
+# def lambda_handler(event, context):
+#     #print("Received event: " + json.dumps(event, indent=2))
+#     try:
+#         print("Event Passed to Handler: " + json.dumps(event))
+#         data = {}
+#         ###############################################
+#         # send curl request to trigger_dag('nba_elt_pipeline_qa') here
+#         url = 'http://ec2-xxx.us-east-2.compute.amazonaws.com:8080/api/experimental/dags/nba_elt_pipeline_qa/dag_runs'
+#         print('sending POST request: ' + url)
+#         r = requests.post(url, json.dumps(data))
+#         print(f"response: {r}")
+#         return r.status_code
+#         ###############################################
+#     except Exception as e:
+#         print(f"An error occured: {e}")
+#         raise e
+
