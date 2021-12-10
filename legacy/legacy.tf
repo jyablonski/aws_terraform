8adb48c3b6d29eb3594e507e2ce0083a78110316
jyablonski9 <jyablonski9@gmail.com>
2021-12-09 20:10:40 -0600

    moving grafana gahbage

--- legacy/legacy.tf
+++ legacy/legacy.tf
@@ -226,3 +226,57 @@ resource "aws_iam_role_policy_attachment" "jacobs_iam_role_update_ses" {
 #     Environment = local.env_type
 #   }
 # }
+
+
+##################
+#                #
+#     GRAFANA    # 
+#                #
+##################
+# Worked, but cost $$ bc it makes requests for cloudwatch metrics every 60s or something
+
+# data "aws_iam_policy_document" "jacobs_grafana_policy" {
+#   statement {
+#     effect = "Allow"
+
+#     principals {
+#       type        = "AWS"
+#       identifiers = ["arn:aws:iam::${local.grafana_account_id}:root"]
+#     }
+
+#     actions = ["sts:AssumeRole"]
+#     condition {
+#       test     = "StringEquals"
+#       variable = "sts:ExternalId"
+#       values   = [var.grafana_external_id]
+#     }
+#   }
+# }
+
+# resource "aws_iam_role" "grafana_labs_cloudwatch_integration" {
+#   name        = "jacobs-grafana-role"
+#   description = "Role used by Grafana CloudWatch Integration."
+
+#   # Allow Grafana Labs' AWS account to assume this role.
+#   assume_role_policy = data.aws_iam_policy_document.jacobs_grafana_policy.json
+
+#   # This policy allows the role to discover metrics via tags and export them.
+#   inline_policy {
+#     name = "jacobs-grafana-role"
+#     policy = jsonencode({
+#       Version = "2012-10-17"
+#       Statement = [
+#         {
+#           Effect = "Allow"
+#           Action = [
+#             "tag:GetResources",
+#             "cloudwatch:GetMetricData",
+#             "cloudwatch:GetMetricStatistics",
+#             "cloudwatch:ListMetrics"
+#           ]
+#           Resource = "*"
+#         }
+#       ]
+#     })
+#   }
+# }
