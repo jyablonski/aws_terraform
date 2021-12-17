70d1dc8e8f48c0611a87d994b9ad70c9a105f1c5
jyablonski9 <jyablonski9@gmail.com>
2021-12-17 17:57:00 -0600

    adding snowflake schemas

--- main.tf
+++ main.tf
@@ -749,8 +749,35 @@ resource "aws_s3_bucket" "jyablonski_tf_cicd_bucket" {
 #                 #
 ###################
 
-resource "snowflake_database" "snowflake_tf_db" {
-  name                        = "snowflake_tf_db"
-  comment                     = "test comment"
+resource "snowflake_database" "NBA_ELT_DB" {
+  name                        = "NBA_ELT_DB"
   data_retention_time_in_days = 0
+}
+
+resource "snowflake_schema" "NBA_AIRFLOW" {
+  database = "NBA_ELT_DB"
+  name     = "NBA_AIRFLOW"
+
+  is_transient        = false
+  is_managed          = false
+  data_retention_days = 0
+}
+
+resource "snowflake_schema" "NBA_AIRFLOW_QA" {
+  database = "NBA_ELT_DB"
+  name     = "NBA_AIRFLOW_QA"
+
+  is_transient        = false
+  is_managed          = false
+  data_retention_days = 0
+}
+
+
+resource "snowflake_schema" "TEST_SCHEMA" {
+  database = "NBA_ELT_DB"
+  name     = "TEST_SCHEMA"
+
+  is_transient        = false
+  is_managed          = false
+  data_retention_days = 0
 }
