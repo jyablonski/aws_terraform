a31c9b0929b9fc5ddc7856d39f3aeab0a1f9b548
jyablonski9 <jyablonski9@gmail.com>
2022-03-18 17:52:06 -0500

    separating files

---
+++ aws_network.tf
@@ -0,0 +1,196 @@
+resource "aws_vpc" "jacobs_vpc_tf" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "Jacobs VPC"
+    Environment = local.env_type
+  }
+}
+
+resource "aws_s3_bucket" "jacobs_bucket_tf" {
+  bucket = "jacobsbucket97"
+
+  tags = {
+    Name        = local.env_name
+    Environment = local.env_type
+  }
+}
+
+# 1 config file per s3 bucket
+# can add things like s3 files -> glacier after 90 days here etc.
+resource "aws_s3_bucket_lifecycle_configuration" "jacobs_bucket_lifecycle_policy" {
+  bucket = aws_s3_bucket.jacobs_bucket_tf.bucket
+
+  rule {
+    expiration {
+      days = 60
+    }
+
+    filter {
+      prefix = "sample_files/"
+    }
+
+    id     = "60-day-removal"
+    status = "Enabled"
+  }
+
+}
+
+resource "aws_s3_bucket_acl" "jyablonski_bucket_tf_acl" {
+  bucket = aws_s3_bucket.jacobs_bucket_tf.id
+  acl    = "private"
+}
+
+# attach this to things like aws lambda or ecs tasks so they can connect to the rds database
+resource "aws_security_group" "jacobs_task_security_group_tf" {
+  name        = "jacobs_security_group for tasks"
+  description = "Connect Tasks to RDS"
+  vpc_id      = aws_vpc.jacobs_vpc_tf.id
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name        = local.env_name
+    Environment = local.env_type
+  }
+}
+
+resource "aws_security_group" "jacobs_rds_security_group_tf" {
+  name        = "jacobs_security_group for rds"
+  description = "Allow Jacobs Traffic to RDS"
+  vpc_id      = aws_vpc.jacobs_vpc_tf.id
+
+  ingress {
+    description = "Custom IP Addresses"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = var.jacobs_cidr_block
+
+  }
+
+  ingress {
+    description = "Open Access"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description     = "Other Security Groups"
+    from_port       = 0
+    to_port         = 0
+    protocol        = -1
+    security_groups = [aws_security_group.jacobs_task_security_group_tf.id]
+  }
+
+
+  # outbound
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name        = local.env_name
+    Environment = local.env_type
+  }
+}
+
+resource "aws_subnet" "jacobs_public_subnet" {
+  vpc_id                  = aws_vpc.jacobs_vpc_tf.id
+  cidr_block              = cidrsubnet(aws_vpc.jacobs_vpc_tf.cidr_block, 8, 1)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "Jacobs Public Subnet"
+    Environment = local.env_type
+  }
+}
+
+resource "aws_subnet" "jacobs_public_subnet_2" {
+  vpc_id                  = aws_vpc.jacobs_vpc_tf.id
+  cidr_block              = cidrsubnet(aws_vpc.jacobs_vpc_tf.cidr_block, 8, 3)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "Jacobs Public Subnet 2"
+    Environment = local.env_type
+  }
+}
+
+resource "aws_db_subnet_group" "jacobs_subnet_group" {
+  name       = "jacobs-subnet-group"
+  subnet_ids = [aws_subnet.jacobs_public_subnet.id, aws_subnet.jacobs_public_subnet_2.id]
+
+  tags = {
+    Name        = local.env_name
+    Environment = local.env_type
+  }
+}
+
+resource "aws_internet_gateway" "jacobs_gw" {
+  vpc_id = aws_vpc.jacobs_vpc_tf.id
+
+  tags = {
+    Name        = "Jacobs Gateway"
+    Environment = local.env_type
+  }
+}
+
+resource "aws_route_table" "jacobs_public_route_table" {
+  vpc_id = aws_vpc.jacobs_vpc_tf.id
+
+  route = [
+    {
+      cidr_block                 = "0.0.0.0/0"
+      gateway_id                 = aws_internet_gateway.jacobs_gw.id
+      carrier_gateway_id         = null
+      destination_prefix_list_id = null
+      egress_only_gateway_id     = null
+      instance_id                = null
+      ipv6_cidr_block            = null
+      local_gateway_id           = null
+      nat_gateway_id             = null
+      network_interface_id       = null
+      transit_gateway_id         = null
+      vpc_endpoint_id            = null
+      vpc_peering_connection_id  = null
+    }
+  ]
+  tags = {
+    Name        = "Jacobs Public Route Table"
+    Environment = local.env_type
+  }
+}
+
+resource "aws_route_table_association" "jacobs_public_route" {
+  subnet_id      = aws_subnet.jacobs_public_subnet.id
+  route_table_id = aws_route_table.jacobs_public_route_table.id
+
+}
+
+resource "aws_route_table_association" "jacobs_public_route_2" {
+  subnet_id      = aws_subnet.jacobs_public_subnet_2.id
+  route_table_id = aws_route_table.jacobs_public_route_table.id
+
+}
