fee09ee18aee5649aabb732644b0aecbeba1e1f4
jyablonski9 <jyablonski9@gmail.com>
2022-07-31 15:51:45 -0500

    tweak dms stuff

---
+++ github.tf
@@ -0,0 +1,76 @@
+resource "aws_iam_openid_connect_provider" "github_provider" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+# ℹ️Note: When GitHub inevitably rotates the certificate for this service, the thumbprint_list value will need to be updated.
+
+data "aws_iam_policy_document" "github_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github_provider.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:jyablonski/kafka_faker_stream:*"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_oidc_role" {
+  name               = "jacobs-github-oidc-role"
+  assume_role_policy = data.aws_iam_policy_document.github_policy.json
+}
+
+resource "aws_iam_policy" "github_oidc_policy" {
+  name        = "jacobs_github_oidc_policy"
+  description = "A Policy for GitHub Actions to write to S3 Bucket using OIDC Credentials"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:PutObject",
+                "s3:DeleteObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${aws_s3_bucket.jacobs_bucket_tf_dev.bucket}/*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${aws_s3_bucket.jacobs_bucket_tf_dev.bucket}"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "jacobs_github_s3_website_user_attachment" {
+  role       = aws_iam_role.github_oidc_role.name
+  policy_arn = aws_iam_policy.github_oidc_policy.arn
+}
