b7d0d3d7fc6b6f3864b55f619d9eeff86e7f7c26
jyablonski9 <jyablonski9@gmail.com>
2023-05-29 13:45:40 -0500

    add the goods

--- elasticsearch.tf
+++ elasticsearch.tf
@@ -87,76 +87,84 @@ resource "aws_cloudwatch_log_resource_policy" "jacobs_es_cluster_logs" {
   policy_name     = "jacobs_es_cluster_logs"
 }
 
-# resource "aws_elasticsearch_domain" "jacobs_opensearch_cluster" {
-#   domain_name           = local.es_cluster_name
-#   elasticsearch_version = "OpenSearch_1.1"
+resource "aws_elasticsearch_domain" "jacobs_opensearch_cluster" {
+  domain_name           = local.es_cluster_name
+  elasticsearch_version = "OpenSearch_2.5"
 
-#   cluster_config {
-#     instance_type            = "t2.small.elasticsearch"
-#     instance_count           = 1
-#     dedicated_master_enabled = false
-#     zone_awareness_enabled   = false
-#   }
+  cluster_config {
+    instance_type            = "t2.small.elasticsearch"
+    instance_count           = 1
+    dedicated_master_enabled = false
+    zone_awareness_enabled   = false
+  }
 
-#   ebs_options {
-#     ebs_enabled = true
-#     volume_size = 10
-#     volume_type = "gp2"
-#   }
+  ebs_options {
+    ebs_enabled = true
+    volume_size = 10
+    volume_type = "gp2"
+  }
 
-#   #   vpc_options {
-#   #     subnet_ids = [
-#   #         aws_subnet.jacobs_public_subnet.id,
-#   #     ]
+  #   vpc_options {
+  #     subnet_ids = [
+  #         aws_subnet.jacobs_public_subnet.id,
+  #     ]
 
-#   #     security_group_ids = [aws_security_group.jacobs_task_security_group_tf.id]
-#   #   }
+  #     security_group_ids = [aws_security_group.jacobs_task_security_group_tf.id]
+  #   }
 
-#   log_publishing_options {
-#     cloudwatch_log_group_arn = aws_cloudwatch_log_group.jacobs_es_cluster_logs.arn
-#     log_type                 = "INDEX_SLOW_LOGS"
-#   }
+  log_publishing_options {
+    cloudwatch_log_group_arn = aws_cloudwatch_log_group.jacobs_es_cluster_logs.arn
+    log_type                 = "INDEX_SLOW_LOGS"
+  }
 
-#   access_policies = <<CONFIG
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Principal": {
-#         "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-#       },
-#       "Action": "es:*",
-#       "Resource": "arn:aws:es:${var.region}:${data.aws_caller_identity.current.account_id}:domain/${local.es_cluster_name}/*"
-#     },
-#     {
-#       "Sid": "",
-#       "Effect": "Allow",
-#       "Principal": {
-#         "AWS": "*"
-#       },
-#       "Action": "es:*",
-#       "Resource": "arn:aws:es:${var.region}:${data.aws_caller_identity.current.account_id}:domain/${local.es_cluster_name}/*",
-#       "Condition": {
-#         "IpAddress": {
-#           "aws:SourceIp": [
-#             "104.153.228.249/32"
-#           ]
-#         }
-#       }
-#     }
-#   ]
-# }
-# CONFIG
+  access_policies = <<CONFIG
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      },
+      "Action": "es:*",
+      "Resource": "arn:aws:es:${var.region}:${data.aws_caller_identity.current.account_id}:domain/${local.es_cluster_name}/*"
+    },
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "es:*",
+      "Resource": "arn:aws:es:${var.region}:${data.aws_caller_identity.current.account_id}:domain/${local.es_cluster_name}/*",
+      "Condition": {
+        "IpAddress": {
+          "aws:SourceIp": [
+            "104.153.228.249/32"
+          ]
+        }
+      }
+    }
+  ]
+}
+CONFIG
 
-#   tags = {
-#     Name        = local.env_name_es
-#     Environment = local.env_type_es
-#     Terraform   = local.terraform_es
-#   }
+  tags = {
+    Name        = local.env_name_es
+    Environment = local.env_type_es
+    Terraform   = local.terraform_es
+  }
 
-#   #   depends_on = [aws_iam_service_linked_role.es_access]
-# }
+  #   depends_on = [aws_iam_service_linked_role.es_access]
+}
+
+resource "aws_route53_record" "jacobs_opensearch_route53_record_api" {
+  zone_id = aws_route53_zone.jacobs_website_zone.zone_id
+  name    = "opensearch.${local.website_domain}"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["${aws_elasticsearch_domain.jacobs_opensearch_cluster.endpoint}"]
+}
 
 # # IN FUTURE - manually create this first as a template and let AWS create the lambda for u
 # # filter pattern is an or or statement to grab all logging.* from python script.  double quotes need a preceding backslash.
--- test_lambda_module.tf
+++ test_lambda_module.tf
@@ -18,52 +18,52 @@
 #   region     = "us-east-1"
 # }
 
-module "lambda_cloudwatch_module" {
-  source               = "./modules/lambda"
-  lambda_name          = "lambda_cloudwatch_scraper"
-  is_lambda_schedule   = true
-  lambda_log_retention = 7
-  lambda_cron          = "cron(30 12 * * ? *)"
-  lambda_runtime       = "python3.9"
-  lambda_memory        = 128
-  lambda_layers = [
-    "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p39-pandas:8",
-    "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p39-requests:8",
-    "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p39-psycopg2-binary:1",
-    "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p39-SQLAlchemy:11"
+# module "lambda_cloudwatch_module" {
+#   source               = "./modules/lambda"
+#   lambda_name          = "lambda_cloudwatch_scraper"
+#   is_lambda_schedule   = true
+#   lambda_log_retention = 7
+#   lambda_cron          = "cron(30 12 * * ? *)"
+#   lambda_runtime       = "python3.9"
+#   lambda_memory        = 128
+#   lambda_layers = [
+#     "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p39-pandas:8",
+#     "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p39-requests:8",
+#     "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p39-psycopg2-binary:1",
+#     "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p39-SQLAlchemy:11"
 
-  ]
-  lambda_env_vars = {
-    webhook_url = "${var.slack_webhook_url}",
-    RDS_USER    = "${var.jacobs_rds_user}",
-    RDS_PW      = "${var.jacobs_rds_pw}",
-    RDS_HOST    = aws_db_instance.jacobs_rds_tf.address,
-    RDS_DB      = "${var.jacobs_rds_db}",
-  }
-  account_id = data.aws_caller_identity.current.account_id
-  region     = "us-east-1"
-}
+#   ]
+#   lambda_env_vars = {
+#     webhook_url = "${var.slack_webhook_url}",
+#     RDS_USER    = "${var.jacobs_rds_user}",
+#     RDS_PW      = "${var.jacobs_rds_pw}",
+#     RDS_HOST    = aws_db_instance.jacobs_rds_tf.address,
+#     RDS_DB      = "${var.jacobs_rds_db}",
+#   }
+#   account_id = data.aws_caller_identity.current.account_id
+#   region     = "us-east-1"
+# }
 
-resource "aws_iam_policy" "cloudwatch_scraper_permissions" {
-  name        = "lambda_cloudwatch_policy"
-  description = "IAM policy for lambda_cloudwatch_scraper"
+# resource "aws_iam_policy" "cloudwatch_scraper_permissions" {
+#   name        = "lambda_cloudwatch_policy"
+#   description = "IAM policy for lambda_cloudwatch_scraper"
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-        "Effect": "Allow",
-        "Action": "cloudwatch:GetMetricData",
-        "Resource": "*",
-        "Sid": "GetMetricDataPermissions"
-    }
-    ]
-}
-EOF
-}
+#   policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#         "Effect": "Allow",
+#         "Action": "cloudwatch:GetMetricData",
+#         "Resource": "*",
+#         "Sid": "GetMetricDataPermissions"
+#     }
+#     ]
+# }
+# EOF
+# }
 
-resource "aws_iam_role_policy_attachment" "cloudwatch_scraper_permissions" {
-  role       = module.lambda_cloudwatch_module.lambda_iam_role_name
-  policy_arn = aws_iam_policy.cloudwatch_scraper_permissions.arn
-}
+# resource "aws_iam_role_policy_attachment" "cloudwatch_scraper_permissions" {
+#   role       = module.lambda_cloudwatch_module.lambda_iam_role_name
+#   policy_arn = aws_iam_policy.cloudwatch_scraper_permissions.arn
+# }
