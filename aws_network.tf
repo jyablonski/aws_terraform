47cb96dc0206c45544e54a163856a07550a91e51
jyablonski9 <jyablonski9@gmail.com>
2023-06-02 20:06:33 -0500

    add

--- aws_network.tf
+++ aws_network.tf
@@ -134,14 +134,14 @@ resource "aws_route_table" "jacobs_public_route_table" {
     carrier_gateway_id         = null
     destination_prefix_list_id = null
     egress_only_gateway_id     = null
-    instance_id                = null
-    ipv6_cidr_block            = null
-    local_gateway_id           = null
-    nat_gateway_id             = null
-    network_interface_id       = null
-    transit_gateway_id         = null
-    vpc_endpoint_id            = null
-    vpc_peering_connection_id  = null
+    # instance_id                = null
+    ipv6_cidr_block           = null
+    local_gateway_id          = null
+    nat_gateway_id            = null
+    network_interface_id      = null
+    transit_gateway_id        = null
+    vpc_endpoint_id           = null
+    vpc_peering_connection_id = null
   }
 
   tags = {
