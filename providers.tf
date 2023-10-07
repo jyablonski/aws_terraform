7fe4bd41d17829f52b5d4dc6b976656227bd2f27
jyablonski9 <jyablonski9@gmail.com>
2023-10-07 09:47:19 -0500

    update provider

--- providers.tf
+++ providers.tf
@@ -24,7 +24,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.0.1"
+      version = "5.11.0"
     }
     postgresql = {
       source  = "cyrilgdn/postgresql"
