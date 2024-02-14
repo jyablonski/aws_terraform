2d53497312a4096b84454f64d52628eeafaf9e45
jyablonski9 <jyablonski9@gmail.com>
2024-02-13 19:25:32 -0600

    add

--- eks.tf
+++ eks.tf
@@ -1,48 +1,55 @@
-# https://www.densify.com/kubernetes-tools/terraform-eks/
+# locals {
+#   eks_cluster_name = "jacobs-eks-cluster"
+#   master_arn = "arn:aws:iam::717791819289:user/jacob"
+# }
 
-# module "eks_k8s1" {
+# module "eks" {
 #   source  = "terraform-aws-modules/eks/aws"
-#   version = "2.3.1"
+#   version = "19.15.3"
 
-#   cluster_version = "1.28"
+#   cluster_name    = local.eks_cluster_name
+#   cluster_version = "1.29"
 
-#   cluster_name = "jacobs-k8s-cluster"
-#   vpc_id       = "vpc-00000000"
+#   vpc_id     = aws_vpc.jacobs_vpc_tf.id
+#   subnet_ids = [aws_subnet.jacobs_public_subnet.id, aws_subnet.jacobs_public_subnet_2.id]
 
-#   subnets = ["subnet-00000001", "subnet-000000002", "subnet-000000003"]
 
-#   cluster_endpoint_private_access = "true"
-#   cluster_endpoint_public_access  = "true"
+#   eks_managed_node_group_defaults = {
+#     ami_type = "AL2_x86_64"
 
-#   write_kubeconfig      = true
-#   config_output_path    = "/.kube/"
-#   manage_aws_auth       = true
-#   write_aws_auth_config = true
+#   }
 
-#   map_users = [
+#   eks_managed_node_groups = {
+#     one = {
+#       name = "node-group-1"
+
+#       instance_types = ["t3.small"]
+
+#       min_size     = 1
+#       max_size     = 3
+#       desired_size = 1
+#     }
+
+#     two = {
+#       name = "node-group-2"
+
+#       instance_types = ["t3.small"]
+
+#       min_size     = 1
+#       max_size     = 2
+#       desired_size = 1
+#     }
+#   }
+
+#   aws_auth_users = [
 #     {
-#       user_arn = "arn:aws:iam::717791819289:user/jacob"
-#       username = "user1"
-#       group    = "system:masters"
-#     },
+#       userarn  = local.master_arn
+#       username = "jacob"
+#       groups   = ["system:masters"]
+#     }
 #   ]
-
-#   worker_groups = [
-#     {
-#       name                 = "workers"
-#       instance_type        = "t2.large"
-#       asg_min_size         = 3
-#       asg_desired_capacity = 3
-#       asg_max_size         = 3
-#       root_volume_size     = 100
-#       root_volume_type     = "gp2"
-#       ebs_optimized        = false
-#       key_name             = "all"
-#       enable_monitoring    = false
-#     },
-#   ]
-
 #   tags = {
-#     Cluster = "k8s"
+#     Environment = "dev"
+#     Terraform   = true
 #   }
 # }
